d31a097646c9365bda24cc79d3ba5b1f050beabf
DouglasBasilio <basilio.douglas@yahoo.com.br>
2020-09-18 23:21:30 -0300

    Formação Java Alura - sintaxes

---
+++ src/TestaValores.java
@@ -0,0 +1,17 @@
+
+public class TestaValores {
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		segundo = primeiro;
+		primeiro = 10;
+		
+		System.out.println(segundo);
+		
+		String saudacao = "Olá, meu nome é ";
+		String nome = "Douglas ";
+		String continuacao = "e minha idade é ";
+		int idade = 30;
+		System.out.println(saudacao+nome+continuacao+idade);
+	}
+}
